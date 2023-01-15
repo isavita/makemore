a95e0f6ae56563de3d60c38fe296407b1afc7d5a
Aleksandar Dimov <isavitaisa@gmail.com>
2023-01-15 23:50:23 +0100

    Version 2

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ N = torch.zeros((27,27), dtype=torch.int32)
 chars = sorted(list(set(".abcdefghijklmnopqrstuvwxyz")))
 s2i = {s:i for i,s in enumerate(chars)}
 i2s = {i:s for s,i in s2i.items()}
-block_size = 4
+block_size = 3
+vocab_size = len(chars)
 
+g = torch.Generator().manual_seed(2147483647)
 def build_dataset(words):
     X, Y = [], []
     for w in words:
@@ -29,66 +31,158 @@ random.seed(42)
 random.shuffle(words)
 n1 = int(0.8*len(words))
 n2 = int(0.9*len(words))
-Xtr, Ytr = build_dataset(words[:n1])
-Xdev, Ydev = build_dataset(words[n1:n2])
-Xte, Yte = build_dataset(words[n2:])
+Xtr, Ytr = build_dataset(words[:n1]) # 80%
+Xdev, Ydev = build_dataset(words[n1:n2]) # 10%
+Xte, Yte = build_dataset(words[n2:]) # 10%
+
+class Linear:
+    def __init__(self, fan_in, fan_out, bias=None):
+        self.weight = torch.randn((fan_in, fan_out), generator=g) / fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+
+    def parameters(self):
+        if self.bias is not None:
+            return [self.weight, self.bias]
+
+        return [self.weight]
+
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        # parameters (trained with backprop)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        # buffers (trained with a running 'momentum update')
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+
+    def __call__(self, x):
+        if self.training:
+            xmean = x.mean(0, keepdim=True) # batch mean
+            xvar = x.var(0, keepdim=True, unbiased=True) # batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean)/ torch.sqrt(xvar + self.eps) # normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var =  (1 - self.momentum) * self.running_var + self.momentum * xvar
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+class Tanh:
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    
+    def parameters(self):
+        return []
+
 # Network setup
-g = torch.Generator().manual_seed(2147483647)
-C = torch.randn((27, 10), generator=g)
-W1 = torch.randn((40, 200), generator=g)
-b1 = torch.randn(200, generator=g)
-W2 = torch.randn((200, 27), generator=g)
-b2 = torch.randn(27, generator=g)
-parameters = [C, W1, b1, W2, b2]
+n_embd = 10
+n_hidden = 100
+C = torch.randn((vocab_size, n_embd), generator=g)
+layers = [
+    Linear(n_embd * block_size, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, vocab_size),
+]
+
+with torch.no_grad():
+    layers[-1].weight *= 0.1
+    #layers[-1].gamma *= 0.1
+    for layer in layers:
+        if isinstance(layer, Linear):
+            layer.weight *= 5/3
+    
+parameters = [C] + [p for layer in layers for p in layer.parameters()]
+# Debug the number of parameters
+print(sum(p.nelement() for p in parameters))
 for p in parameters:
     p.requires_grad = True
 
-# Debug the number of parameters
-print(sum(p.nelement() for p in parameters))
+steps = 100000
+batch_size = 32
 
-# lre = torch.linspace(-3, 0, 1000)
-# lrs = 10**lre
-for i in range(100000):
+for i in range(steps):
     # minibatch construct
-    ix = torch.randint(0, Xtr.shape[0], (32,))
+    ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+    Xb, Yb = Xtr[ix], Ytr[ix] # batch X, Y
+
     # forward pass
-    emb = C[Xtr[ix]]
-    h = torch.tanh(emb.view(-1, 40) @ W1 + b1)
-    logits = h @ W2 + b2
-    loss = F.cross_entropy(logits, Ytr[ix])
+    emb = C[Xb] # embed the characters into vectors
+    x = emb.view(emb.shape[0], -1) # concatenate the vectors
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, Yb)
 
     # backward pass 
+    for layer in layers:
+        layer.out.retain_grad()
     for p in parameters:
         p.grad = None
     loss.backward()
+    
     # update
-    lr = 0.01 if i > 50000 else 0.1
+    lr = 0.1 if i < steps / 2 else 0.01
     for p in parameters:
         p.data += -lr * p.grad
 
-def validate():
-    emb = C[Xdev]
-    h = torch.tanh(emb.view(-1, 40) @ W1 + b1)
-    logits = h @ W2 + b2
-    loss = F.cross_entropy(logits, Ydev)
-    print(loss)
+@torch.no_grad()
+def split_loss(split):
+    x,y = {
+    'train': (Xtr, Ytr),
+    'val': (Xdev, Ydev),
+    'test': (Xte, Yte),
+    }[split]
+
+    emb = C[x] # (N, block_size, n_embd)
+    x = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, y)
+    print(split, loss.item())
+
 
 def sample():
     for _ in range(20):
         out = []
-        context = [0] * block_size
+        context = [0] * block_size # initialize with all ...
         while True:
-            emb = C[torch.tensor([context])]
-            h = torch.tanh(emb.view(1, -1) @ W1 + b1)
-            logits = h @ W2 + b2
+            # forward pass the neural net
+            emb = C[torch.tensor([context])] # (1,block_size,n_embd)
+            x = emb.view(emb.shape[0], -1) # concatenate the vectors
+            for layer in layers:
+                x = layer(x)
+            logits = x
             probs = F.softmax(logits, dim=1)
+            # sample from the distribution
             ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+            # shift the context window and track the samples
             context = context[1:] + [ix]
             out.append(ix)
+            # if we sample the special '.' token, break
             if ix == 0:
                 break
         print("".join(i2s[i] for i in out[:-1]))
+
 sample()
-validate()
+split_loss("train")
+split_loss("val")
 # training, validation, test
 # 80%, 10%, 10%
