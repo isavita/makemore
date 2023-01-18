ca1f7faf8e8c109a8772fb1ab12ab51758326c4d
Aleksandar Dimov <isavitaisa@gmail.com>
2023-01-18 20:34:02 +0100

    Manual backpropagation

--- main.py
+++ main.py
@@ -88,100 +88,180 @@ class Tanh:
     def parameters(self):
         return []
 
+def cmp(s, dt, t):
+    ex = torch.all(dt == t.grad).item()
+    app = torch.allclose(dt, t.grad)
+    maxdiff = (dt - t.grad).abs().max().item()
+    print(f'{s:15s} | exact: {str(ex):5s} | approximate: {str(app):5s} | maxdiff: {maxdiff}')
+
 # Network setup
 n_embd = 10
-n_hidden = 100
+n_hidden = 64
 C = torch.randn((vocab_size, n_embd), generator=g)
-layers = [
-    Linear(n_embd * block_size, n_hidden), BatchNorm1d(n_hidden), Tanh(),
-    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
-    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
-    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
-    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
-    Linear(n_hidden, vocab_size), BatchNorm1d(vocab_size),
-]
 
-with torch.no_grad():
-    layers[-1].gamma *= 0.1
-    for layer in layers[:-1]:
-        if isinstance(layer, Linear):
-            layer.weight *= 5/3
-    
-parameters = [C] + [p for layer in layers for p in layer.parameters()]
-# Debug the number of parameters
-print(sum(p.nelement() for p in parameters))
+# Layer1
+W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * (5/3)/((n_embd * block_size)**0.5)
+b1 = torch.randn(n_hidden, generator=g) * 0.1
+# Layer2
+W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.1
+b2 = torch.randn(vocab_size, generator=g) * 0.1
+# BarchNorm parameters 
+bngain = torch.randn((1, n_hidden)) * 0.1 + 1.0
+bnbias = torch.randn((1, n_hidden)) * 0.1
+parameters = [C, W1, b1, W2, b2, bngain, bnbias]
 for p in parameters:
     p.requires_grad = True
 
-steps = 100000
+# debug
+print(sum(p.nelement() for p in parameters))
+
 batch_size = 32
+n = batch_size
+ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+Xb, Yb = Xtr[ix], Ytr[ix] # batch X, Y
 
-for i in range(steps):
-    # minibatch construct
-    ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
-    Xb, Yb = Xtr[ix], Ytr[ix] # batch X, Y
+# forward pass
+emb = C[Xb] # embed the characters into vectors
+embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
+# Linear layer 1
+hprebn = embcat @ W1 + b1 # hidden layer pre-activation
+# BatchNorm layer
+bnmeani = 1/n*hprebn.sum(0, keepdim=True)
+bndiff = hprebn - bnmeani
+bndiff2 = bndiff**2
+bnvar = 1/(n-1)*(bndiff2).sum(0, keepdim=True)
+bnvar_inv = (bnvar + 1e-5)**-0.5
+bnraw = bndiff * bnvar_inv
+hpreact = bngain * bnraw + bnbias
+# Non-linearity
+h = torch.tanh(hpreact) # hidden layer
+# Linear layer 2
+logits = h @ W2 + b2 # output layer
+# cross entropy loss (same as F.cross_entropy(logits, Yb))
+logit_maxes = logits.max(1, keepdim=True).values
+norm_logits = logits - logit_maxes # subtract max for numerical stability
+counts = norm_logits.exp()
+counts_sum = counts.sum(1, keepdims=True)
+counts_sum_inv = counts_sum**-1 
+probs = counts * counts_sum_inv
+logprobs = probs.log()
+loss = -logprobs[range(n), Yb].mean()
 
-    # forward pass
-    emb = C[Xb] # embed the characters into vectors
-    x = emb.view(emb.shape[0], -1) # concatenate the vectors
-    for layer in layers:
-        x = layer(x)
-    loss = F.cross_entropy(x, Yb)
+# PyTorch backward pass
+for p in parameters:
+    p.grad = None
+for t in [logprobs, probs, counts, counts_sum, counts_sum_inv, norm_logits,
+            logit_maxes, logits, h, hpreact, bnraw, bnvar_inv, bnvar, bndiff2, bndiff,
+            hprebn, bnmeani, embcat, emb]:
+    t.retain_grad()
+loss.backward()
+loss
 
-    # backward pass 
-    for layer in layers:
-        layer.out.retain_grad()
-    for p in parameters:
-        p.grad = None
-    loss.backward()
-    
-    # update
-    lr = 0.1 if i < steps / 2 else 0.01
-    for p in parameters:
-        p.data += -lr * p.grad
+dlogprobs = torch.zeros_like(logprobs)
+dlogprobs[range(n), Yb] = -1.0/n
+dprobs = (1/probs) * dlogprobs
+dcounts_sum_inv = (counts * dprobs).sum(1, keepdim=True)
+dcounts = counts_sum_inv * dprobs
+dcounts_sum = (-counts_sum**-2) * dcounts_sum_inv
+dcounts += torch.ones_like(counts) * dcounts_sum
+dnorm_logits = counts * dcounts
+dlogits = dnorm_logits.clone()
+dlogit_maxes = (-dnorm_logits).sum(1, keepdim=True)
+dlogits += F.one_hot(logits.max(1).indices, num_classes=logits.shape[1]) * dlogit_maxes
+dh = dlogits @ W2.T
+dW2 = h.T @ dlogits
+db2 = dlogits.sum(0)
+dhpreact = (1.0 - h**2) * dh
+dbnraw = bngain * dhpreact
+dbngain = (bnraw * dhpreact).sum(0, keepdim=True)
+dbnraw = bngain * dhpreact
+dbnbias = dhpreact.sum(0, keepdim=True)
+dbndiff = bnvar_inv * dbnraw
+dbnvar_inv = (bndiff * dbnraw).sum(0, keepdim=True)
+dbnvar = (-0.5*(bnvar + 1e-5)**-1.5) * dbnvar_inv
+dbndiff2 = (1.0/(n-1))*torch.ones_like(bndiff2) * dbnvar
+dbndiff += (2*bndiff) * dbndiff2
+dhprebn = dbndiff.clone()
+dbnmeani = (-dbndiff).sum(0)
+dhprebn += 1.0/n * (torch.ones_like(hprebn) * dbnmeani)
+dembcat = dhprebn @ W1.T
+dW1 = embcat.T @ dhprebn
+db1 = dhprebn.sum(0)
+demb = dembcat.view(emb.shape)
+dC = torch.zeros_like(C)
+for k in range(Xb.shape[0]):
+    for j in range(Xb.shape[1]):
+        ix = Xb[k, j]
+        dC[ix] += demb[k, j]
+cmp('logprobs', dlogprobs, logprobs)
+cmp('probs', dprobs, probs)
+cmp('counts_sum_inv', dcounts_sum_inv, counts_sum_inv)
+cmp('counts_sum', dcounts_sum, counts_sum)
+cmp('counts', dcounts, counts)
+cmp('norm_logits', dnorm_logits, norm_logits)
+cmp('logit_maxes', dlogit_maxes, logit_maxes)
+cmp('logits', dlogits, logits)
+cmp('h', dh, h)
+cmp('W2', dW2, W2)
+cmp('b2', db2, b2)
+cmp('hpreact', dhpreact, hpreact)
+cmp('bngain', dbngain, bngain)
+cmp('bnbias', dbnbias, bnbias)
+cmp('bnraw', dbnraw, bnraw)
+cmp('bnvar_inv', dbnvar_inv, bnvar_inv)
+cmp('bnvar', dbnvar, bnvar)
+cmp('bndiff2', dbndiff2, bndiff2)
+cmp('bndiff', dbndiff, bndiff)
+cmp('bnmeani', dbnmeani, bnmeani)
+cmp('hprebn', dhprebn, hprebn)
+cmp('embcat', dembcat, embcat)
+cmp('W1', dW1, W1)
+cmp('b1', db1, b1)
+cmp('emb', demb, emb)
+cmp('C', dC, C)
 
-@torch.no_grad()
-def split_loss(split):
-    x,y = {
-    'train': (Xtr, Ytr),
-    'val': (Xdev, Ydev),
-    'test': (Xte, Yte),
-    }[split]
+# @torch.no_grad()
+# def split_loss(split):
+#     x,y = {
+#     'train': (Xtr, Ytr),
+#     'val': (Xdev, Ydev),
+#     'test': (Xte, Yte),
+#     }[split]
 
-    emb = C[x] # (N, block_size, n_embd)
-    x = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
-    for layer in layers:
-        x = layer(x)
-    loss = F.cross_entropy(x, y)
-    print(split, loss.item())
+#     emb = C[x] # (N, block_size, n_embd)
+#     x = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
+#     for layer in layers:
+#         x = layer(x)
+#     loss = F.cross_entropy(x, y)
+#     print(split, loss.item())
 
-for layer in layers:
-  layer.training = False
+# for layer in layers:
+#   layer.training = False
 
-def sample():
-    for _ in range(20):
-        out = []
-        context = [0] * block_size # initialize with all ...
-        while True:
-            # forward pass the neural net
-            emb = C[torch.tensor([context])] # (1,block_size,n_embd)
-            x = emb.view(emb.shape[0], -1) # concatenate the vectors
-            for layer in layers:
-                x = layer(x)
-            logits = x
-            probs = F.softmax(logits, dim=1)
-            # sample from the distribution
-            ix = torch.multinomial(probs, num_samples=1, generator=g).item()
-            # shift the context window and track the samples
-            context = context[1:] + [ix]
-            out.append(ix)
-            # if we sample the special '.' token, break
-            if ix == 0:
-                break
-        print("".join(i2s[i] for i in out[:-1]))
+# def sample():
+#     for _ in range(20):
+#         out = []
+#         context = [0] * block_size # initialize with all ...
+#         while True:
+#             # forward pass the neural net
+#             emb = C[torch.tensor([context])] # (1,block_size,n_embd)
+#             x = emb.view(emb.shape[0], -1) # concatenate the vectors
+#             for layer in layers:
+#                 x = layer(x)
+#             logits = x
+#             probs = F.softmax(logits, dim=1)
+#             # sample from the distribution
+#             ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+#             # shift the context window and track the samples
+#             context = context[1:] + [ix]
+#             out.append(ix)
+#             # if we sample the special '.' token, break
+#             if ix == 0:
+#                 break
+#         print("".join(i2s[i] for i in out[:-1]))
 
-sample()
-split_loss("train")
-split_loss("val")
+# sample()
+# split_loss("train")
+# split_loss("val")
 # training, validation, test
 # 80%, 10%, 10%
